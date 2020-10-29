265a3958a0f28977d5c9519ae5188cb460abe9c3
Jose Oswaldo Sanchez Gutierrez <re3nemesis@hotmail.com>
2020-10-28 22:21:20 -0600

    Api working

---
+++ DataAccess/Models/Contexts/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Models.Contexts
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- TestProjectAPI/Startup.cs
+++ TestProjectAPI/Startup.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess.Models;
+using DataAccess.Models.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace TestProjectAPI
 {
@@ -25,18 +29,69 @@ namespace TestProjectAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("ToysGames"));
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "TOYSGAMES API SERVICE",
+                    Description = "A service backend for TOYSGAMES Application",
+                    TermsOfService = new Uri("https://github.com/grayfoxgfx"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "grayfoxgfx",
+                        Url = new Uri("https://github.com/grayfoxgfx")
+                    }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+            });
+            // Add CORS services
+            services.AddCors(o => o.AddPolicy("AddCorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            var context = serviceProvider.GetService<ApiContext>();
+            AddTestData(context);
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TOYSGAMES_API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseCors("AddCorsPolicy");
 
             app.UseRouting();
 
@@ -46,6 +101,22 @@ namespace TestProjectAPI
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void AddTestData(ApiContext context)
+        {
+            var product1 = new Product()
+            {
+                Id = 1,
+                AgeRestriction= 15,
+                Company= "Hasbro",
+                Description = "Buzz Light Year action figure",
+                Name = "Buzz Light Year",
+                Price = 212.50M
+            };
+
+            context.Products.Add(product1);
+            context.SaveChanges();
         }
     }
 }
