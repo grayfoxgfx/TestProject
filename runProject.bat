3fc5a17d28fd752b24ecd2ee8541a7d7a4086294
grayfoxgfx <re3nemesis@hotmail.com>
2025-02-17 02:31:19 -0600

    Fix docker file

--- runProject.bat
+++ runProject.bat
@@ -2,4 +2,9 @@ cd "ToysGamesFront\ClientApp"
 start ng serve
 
 cd "../../TestProjectAPI"
-start dotnet run --urls=https://localhost:5001/
+start docker build --rm -t test-project:latest -f Dockerfile .
+start docker run --rm -p 5000:5000 -p 5001:5001 `
+  -e ASPNETCORE_URLS="http://+:5000;https://+:5001" `
+  -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx `
+  -e ASPNETCORE_Kestrel__Certificates__Default__Password="password" `
+  -v "$env:USERPROFILE\.aspnet\https:/https:ro" test-project:latest
