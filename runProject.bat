21ef4ec8c2601e53554d0449826c2c8916d7e48b
grayfoxgfx <re3nemesis@hotmail.com>
2025-02-17 03:04:57 -0600

    Fix auto run

--- runProject.bat
+++ runProject.bat
@@ -1,10 +1,24 @@
+@echo off
 cd "ToysGamesFront\ClientApp"
-start ng serve
+start cmd /k ng serve
 
-cd "../../TestProjectAPI"
-start docker build --rm -t test-project:latest -f Dockerfile .
-start docker run --rm -p 5000:5000 -p 5001:5001 `
-  -e ASPNETCORE_URLS="http://+:5000;https://+:5001" `
-  -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx `
-  -e ASPNETCORE_Kestrel__Certificates__Default__Password="password" `
-  -v "$env:USERPROFILE\.aspnet\https:/https:ro" test-project:latest
+cd "../../"
+
+:: Ensure we are in the correct directory
+cd /d "%~dp0\TestProject"
+
+:: Check if the Dockerfile exists
+if not exist Dockerfile (
+    echo ERROR: Dockerfile not found in %CD%
+    exit /b 1
+)
+
+:: Build the Docker image and wait for completion
+docker build --rm -t test-project:latest -f Dockerfile .
+
+:: Run the Docker container
+docker run --rm -p 5000:5000 -p 5001:5001 ^
+  -e ASPNETCORE_URLS="http://+:5000;https://+:5001" ^
+  -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx ^
+  -e ASPNETCORE_Kestrel__Certificates__Default__Password="password" ^
+  -v "%USERPROFILE%\.aspnet\https:/https:ro" test-project:latest
