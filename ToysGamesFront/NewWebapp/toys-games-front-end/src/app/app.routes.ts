b12365108957bb54afa6773ebae5bd6ce9a47827
grayfoxgfx <re3nemesis@hotmail.com>
2025-02-18 13:27:22 -0600

    Delete unnesessary files

--- ToysGamesFront/NewWebapp/toys-games-front-end/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'products', component: ProductsComponent },
-];
--- ToysGamesFront/NewWebapp/toys-games-front-end/src/app/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  standalone: true, // ✅ Add this line
-  templateUrl: './home.component.html',
-})
-export class HomeComponent {
-}
